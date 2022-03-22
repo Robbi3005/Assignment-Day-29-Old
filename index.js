71149bb203ee72d120500ae2fefb336bb610af47
Robbi3005 <97607694+Robbi3005@users.noreply.github.com>
2022-03-22 20:33:11 +0700

    add code delete

--- index.js
+++ index.js
@@ -43,22 +43,69 @@ app.get('/store-new', (req, res) => {
     res.status(200).send(JSON.parse(fs.readFileSync('new-data.json')));
 })
 
+//----------------------------------------------------------------------------
+
+app.get('/store-new/:id', (req, res) => {
+    
+    const id = req.params.id
+
+    const data = JSON.parse(fs.readFileSync('new-data.json'));
+
+    let foundData = null // ga boleh pake const
+
+    for (let i = 0; i < data.length; i++) {
+
+        if (data[i].id == id) {
+            foundData = data[i];
+            break;
+        }
+    }
+
+    res.status(200).json(foundData);
+})
+
+//----------------------------------------------------------------------------
+
+app.get('/store-new-delete', (req, res) => {
+    res.status(200).send(JSON.parse(fs.readFileSync('new-data-delete.json')));
+})
+
 //---------------------------------------------------------------------------
 
 app.put('/store/:id', (req, res) => {
     const data = JSON.parse(fs.readFileSync('new-data.json'));
 
     for (let i = 0; i < data.length; i++) {
-        
+
         if (data[i].id == req.params.id) {
             data[i] = req.body;
-            // ini buat nmaba file baru di vscode
+
             fs.writeFileSync('new-data.json', JSON.stringify(data, null, 2))
             break;
         }
     }
     res.status(200).json(req.body)
 })
+
+//---------------------------------------------------------------------------
+
+app.delete('/store-delete/:id', function (req, res) {
+    // find item from array of data
+
+    const data = JSON.parse(fs.readFileSync('new-data.json'));
+
+    for (let i = 0; i < data.length; i++) {
+
+        if (data[i].id == req.params.id) {
+            // data.splice(data[i].id)
+            data.splice(i, 1)
+
+            fs.writeFileSync('new-data-delete.json', JSON.stringify(data, null, 2))
+            break;
+        }
+    }
+    res.sendStatus(204);
+});
 
 //---------------------------------------------------------------------------
 
